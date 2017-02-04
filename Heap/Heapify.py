ede0a0e667da8d4137038184599eeedca68a8130
Ayush Mittal <ayus097836@gmail.com>
2017-02-04 09:21:43 +0530

    Slight changes to make heap-sort run faster

--- Heap/Heapify.py
+++ Heap/Heapify.py
@@ -1,36 +1,40 @@
-def max_heap(A,parent,length):
+#Create Heap
+def max_heapify(alist,length,parent):
+    left=2*parent+1
+    if left>length:
+        return
+    if left<length and alist[left]<alist[left+1]:
+        left=left+1
+    if alist[left]>alist[parent]:
+        swap(alist,left,parent)
+        parent=left
+        left=2*parent+1
+        max_heapify(alist,length,parent)
+        
+
+def swap(alist,left,parent):
+    temp=alist[left]
+    alist[left]=alist[parent]
+    alist[parent]=temp
+
+def build_Max_heap(alist,length):
+    for i in range(length/2-1,-1,-1):
+        max_heapify(alist,length,i)
     
-        left=parent*2+1
-        if left>length:
-            return
-        if left<length and A[left]<A[left+1]:
-            left=left+1
-        if A[left]>A[parent]:
-            swap(A,left,parent)
-            parent=left
-            left=2*parent+1
-            max_heap(A,parent,length)
 
-
-def swap(A,L,P):
-    temp=0
-    temp=A[L]
-    A[L]=A[P]
-    A[P]=temp
-
-
-def build_max_heap(A,length):
-    for i in xrange(length/2,-1,-1):
-        max_heap(A,i,length)
-
-def HeapSort(A,length):
+def heap_sort(alist,length):
     if length<1:
         return
-    build_max_heap(A,length)
-    swap(A,0,length)
-    HeapSort(A,length-1)
+    max_heapify(alist,length,0) 
+    swap(alist,0,length)
+    heap_sort(alist,length-1)
 
-A=[16,6,14,8,7,9,3,2,4,1,3,5,2,6,4,7,34,23,45,11,23]
+A=[16,6,4,8,7,39,3,2,4,1,3,5,2,41,4,7,34,23,45,11,23]
 length=len(A)-1
-HeapSort(A,length)
+build_Max_heap(A,length)
+heap_sort(A,length)
 print A
+
+
+    
+   
