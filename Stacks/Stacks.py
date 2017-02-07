b4804760e3a33d4ef4b2a29f074440f5513096b6
Ayush Mittal <ayus097836@gmail.com>
2017-02-08 00:51:04 +0530

    Addition of max funciton in stack

--- Stacks/Stacks.py
+++ Stacks/Stacks.py
@@ -2,21 +2,33 @@
 class Stack:
     def __init__(self):
         self.items=[]
+        self.max=0
 
     def __str__(self):
         return self.items
 
     def push(self,key):
         self.items.append(key)
+        if key>self.max:
+            self.max=key
 
     def isEmpty(self):
         return self.items==[]
 
     def pop(self):
-        return self.items.pop()
+        top=self.items.pop()
+        if top==self.max:
+            if len(self.items)>0:
+                self.max=max(self.items)
+            else:
+                self.max=0
+        return top
 
     def peep(self):
         return self.items[len(self.items)-1]
+
+    def getMax(self):
+        return self.max
 
 """
 s=Stack()
