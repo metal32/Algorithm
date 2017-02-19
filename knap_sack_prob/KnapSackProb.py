49574719d6c140efc1a5f6123536f4adfa2e76d6
Ayush Mittal <ayus097836@gmail.com>
2017-02-19 12:30:51 +0530

    Slight changes in knapsack memorization code

--- knap_sack_prob/KnapSackProb.py
+++ knap_sack_prob/KnapSackProb.py
@@ -1,28 +1,11 @@
-### DECISION TREE- DEPTH FIRST LEFT FIRST, Complexicity(2**n)
+### DECISION TREE- DEPTH FIRST LEFT FIRST, Complexicity(2**n), without dynamic programming
+### With dynamic programming the complexicity comes into the form of pseudopolynomial, i.e. O(n*S)
 ### You arre given weiht and value of 3 objects with a constraing of maximum weight you can carry, so you have to find the maximum vaue.
 w=[1,5,3,4,5,6,2,3,4]
 v=[15,10,9,23,10,4,7,19,10]
 #Constrain is that maximum weight you can carry is 10 only.
-"""
-def maxVal(w,v,i,aw):
-    print "maxval called with i ",i," and ",aw
-    global numCalls
-    numCalls+=1
-    if i==0:
-        if w[i]<=aw:
-            return v[i]
-        else: return 0
-    without_i=maxVal(w,v,i-1,aw)
-    print i
-    if w[i]>aw:
-        return without_i
-    else:
-        with_i=v[i]+maxVal(w,v,i-1,aw-w[i])
-    return max(without_i,with_i)
-numCalls=0
-r=maxVal(w,v,len(w)-1,8)
-print "The maximum value earned ",r," in number of calls ",numCalls
-"""
+
+## With memorization 
 def fastmaxVal(w,v,i,aw,m):
     global numCalls
     numCalls+=1
@@ -48,4 +31,26 @@ def maxVal(w,v,i,aw):
     m={}
     return fastmaxVal(w,v,i,aw,m)
 numCalls=0
-print maxVal(w,v,len(w)-1,5)
+print maxVal(w,v,len(w)-1,10)
+
+
+"""
+def maxVal(w,v,i,aw):
+    print "maxval called with i ",i," and ",aw
+    global numCalls
+    numCalls+=1
+    if i==0:
+        if w[i]<=aw:
+            return v[i]
+        else: return 0
+    without_i=maxVal(w,v,i-1,aw)
+    print i
+    if w[i]>aw:
+        return without_i
+    else:
+        with_i=v[i]+maxVal(w,v,i-1,aw-w[i])
+    return max(without_i,with_i)
+numCalls=0
+r=maxVal(w,v,len(w)-1,8)
+print "The maximum value earned ",r," in number of calls ",numCalls
+"""
--- knap_sack_prob/readme.txt
+++ knap_sack_prob/readme.txt
@@ -1,2 +1,7 @@
-knapsack problem using greedy algorithm. There are two codes given, one simple brute force
-while the other one using Dynamic Programming memorization concept.
+KnapSack Problem two solutions
+a) one using Dynamic Programming memorization concept.
+b) Using bottom to up appoach
+
+the order of complexity is Pseudo polynomial for knapSack in order of O(n*S) where n is the number of items 
+S is the weight constraint.
+
