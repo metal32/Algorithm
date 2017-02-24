db66e12e354ecea30fd74bd90e1ecf29fcf5b88b
Ayush Mittal <ayus097836@gmail.com>
2017-02-24 17:28:38 +0530

    Changes in knapSack DP

--- knap_sack_prob/knap_sack_2.py
+++ knap_sack_prob/knap_sack_2.py
@@ -22,7 +22,22 @@ def knapSack(n,S,s,v):
                 if j>=s[i]:
                     choices.append(dp[i+1][j-s[i]]+v[i])
                 dp[i][j]=max(choices)
-    return dp[0][S]
+
+    o=[]
+    def compute_sol(i,j):
+        if i<n:
+            if j>=w[i]:
+                if dp[i][j-w[i]]+v[i]>dp[i+1][j]:
+                    o.append(i)
+                    compute_sol(i+1,j-s[i])
+                else:
+                    compute_sol(i+1,j)
+            else:
+                compute_sol(i+1,j)
+
+    compute_sol(0,S)
+    # return the maximum value of the sack and the indexes which we choose. 
+    return dp[0][S], o
  
 print knapSack(n,S,s,v)
 
