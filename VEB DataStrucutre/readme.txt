f88bc9141a111cc773332eb5df2d7f69fdb95635
Ayush Mittal <ayus097836@gmail.com>
2017-03-12 16:51:22 +0530

    Slight addition in VEB data structure for using it in odd powers of 2

--- VEB DataStrucutre/readme.txt
+++ VEB DataStrucutre/readme.txt
@@ -1,23 +1,20 @@
 '''                                                           Van Embde Boas Trees Theory                                            '''
-# One of the most used data-structures in network routing tables O(loglogu) complexity for insert, delete and successor operations
-# It is exponential times faster than balanced binary search trees
-
-## It is not practically used in many places despite being very good running time as the running time is for universe so 
-## If we are not having many elements especially in the range of 2**20 then this algo will be very inefficient as the universe is 2**32
-
-""" General STEPS """
-# Bit vector of size u O=Absent and 1=Present
-# Split universe in square root of u (sqrt(u)) clusters, of size sqrt(u)
-# Summary vector is the list of all the decisions of all the clusters, so if there are total 4 clusters then summary vector size will be 4.
-# For ex u=16 now we have divided it into 4 cluster of size 4 eac h 
-# and the summary vector has the decision of all the clusters so it will be of size 4
-
-## So any integer can be represented in the form of  x=i*sqrt(u)+j, where i the cluseter and j is the element in the cluster
-## So 9 can be represented as =2*4+1 ; where u =16 so sqrt(u)=4 so i=2 and j=1 where cluster and its elements index starts from 0.
-
-''' Steps for inserting '''
-##for inserting a number first find the cluster by diving the number by sqrt(u) like what we did in our above example =>  high(x)=x//sqrt(u)
-## and the remainder to be the position to be stored in that cluster => low(x)=x%sqrt(u) 
+This data structure support priority queues operations and the running time is O (loglog n) 
+while the size of the key is restricted in the range of 0 to n-1 and there can’t be any duplicated keys.
+In it instead of superimposing a binary tree on top of the bit vector, we superimpose a tree of degree sqrt (u). 
+We use recursive structure. Starting with a universe of size u, we make a structure having u1/2 items which themselves 
+holds u1/4 items, which holds structure of u1/8 items and so on, down to base size of 2.
+For finding minimum (maximum) you just move to the left (right) most entry having 1 in the summary and then 
+do the linear search in cluster for the leftmost (rightmost) point.
+For finding Successor (predecessor) you should move to the right (left) in the same cluster if you find an entry with 1 
+then it is done otherwise let i=x/sqrt(u) search right(left) of this index in summary and the first position that holds 
+1 gives the index of the cluster, search within the cluster for the leftmost (rightmost) point.
+We use min and max value to store in each vEB tree to reduce the recursive calls for finding successor and
+predecessor and we can easily find min and max too.
+Size of u will be power 2k.
+Note: Easy explanation of vEB: just think that the number of bit b that you have to store at each level reduced by half,
+so the number of level will be log b until it reaches to 1 bit and as we already know b= log u 
+so overall complexity is (loglog u).
 
 
 
--- VEB DataStrucutre/vEB_DataStructure.py
+++ VEB DataStrucutre/vEB_DataStructure.py
@@ -1,140 +1,209 @@
+'''
+Created on 9-Mar-2017
+
+@author: Ayush
+@ Reference: CLRS and https://github.com/erikwaing/VEBTree/blob/master/VEB.py
+
+'''
+
+'''                                                           Van Embde Boas Trees Theory                                            '''
+# VEB tree of any power 2 whether it is even or odd can be stored in this
+# data-structure
+
 import math
+
+
 class VEB(object):
-    def __init__(self,u):
-        if u<0:
-            raise Exception ('u cannot be less than 0')
-        self.u=2
-        while self.u<u:
-            self.u*=self.u
-        self.min=None
-        self.max=None
-        if u>2:
-            self.clusters=[None for i in range(self.high(self.u))]
-            self.summary=None
 
-    def high(self,x):
-        return int(math.floor(x/math.sqrt(self.u)))
+    def __init__(self, u):
+        if u < 0:
+            raise Exception(' u cannot be less than 0')
+        self.u = 2
+        while self.u < u:
+            self.u = self.u * 2
+        self.min = None
+        self.max = None
+        if u > 2:
+            self.clusters = [
+                None for _ in range(int(self.highroot(self.u)))]
+            self.summary = None
 
-    def low(self,x):
-        return (x%math.ceil(math.sqrt(self.u)))
+    def lowroot(self, x):
+        m = 1
+        while 2**m < x:
+            m += 1
+        if m % 2 != 0:
+            return math.sqrt(2**(m - 1))
+        else:
+            return math.sqrt(2**m)
 
-    def index(self,x,y):
-        return int((x*math.floor(math.sqrt(self.u)))+y)
+    def highroot(self, x):
+        m = 1
+        while 2**m < x:
+            m += 1
+        if m % 2 != 0:
+            return int(math.sqrt(2**(m + 1)))
+        else:
+            return int(math.sqrt(2**m))
 
-    def emptyInsert(self,x):
-        self.min=x
-        self.max=x
+    def __str__(self):
+        return 'It\'s size is {} and max cluster is: {} and min cluster is: {}'.format(self.u, self.max, self.min)
 
-    def insert(self,x):
-        if self.min==None:
+    def high(self, x):
+        return int(math.floor(x // self.lowroot(self.u)))
+
+    def low(self, x):
+        return int(x % self.lowroot(self.u))
+
+    def index(self, x, y):
+        return int(x * self.lowroot(self.u) + y)
+
+    def emptyInsert(self, x):
+        self.min = x
+        self.max = x
+
+    def insert(self, x):
+        if self.min == None:
             self.emptyInsert(x)
         else:
-            if x<self.min:
-                self.min,x=x,self.min
-            if self.u>2:
-                h=self.high(x)
-                if self.clusters[h]==None:
-                    self.clusters[h]=VEB(self.high(self.u))
+            if x < self.min:
+                self.min, x = x, self.min
+            if self.u > 2:
+                h = self.high(x)
+                if self.clusters[h] == None:
+                    self.clusters[h] = VEB(self.high(self.u))
                 if self.summary == None:
-                    self.summary=VEB(self.high(self.u))
-                if self.clusters[h].min==None:
+                    self.summary = VEB(self.high(self.u))
+                if self.clusters[h].min == None:
                     self.summary.insert(h)
                     self.clusters[h].emptyInsert(self.low(x))
                 else:
                     self.clusters[h].insert(self.low(x))
-            if x>self.max:
-                self.max=x
-                  
-    def successor(self,x):
-        if self.u<=2:
-            if x==0 and self.max==1:
+
+            if x > self.max:
+                self.max = x
+
+    def member(self, x):
+        if x == self.min or x == self.max:
+            return True
+        elif self.u <= 2:
+            return False
+        else:
+            cluster = self.clusters[self.high(x)]
+            if cluster != None:
+                return cluster.member(self.low(x))
+            else:
+                return False
+
+    def successor(self, x):
+        if self.u <= 2:
+            if x == 0 and self.max == 1:
                 return 1
             else:
                 return None
-        elif self.min!=None and x<self.min:
+        elif self.min != None and x < self.min:
             return self.min
         else:
-            h=self.high(x)
-            l=self.low(x)
-            maxLow=None
-            clusters=self.clusters[h]
-            if clusters!=None:
-                maxLow=cluster.max
-            if maxLow!=None and l<maxLow:
-                offset=cluster.successor(l)
-                return self.index(h,offset)
+            h = self.high(x)
+            l = self.low(x)
+            maxlow = None
+            cluster = self.clusters[h]
+            if cluster != None:
+                maxlow = cluster.max
+            if maxlow != None and l < maxlow:
+                offset = cluster.successor(l)
+                return self.index(h, offset)
             else:
-                succ_cluster=None
-                if self.summary!=None:
-                    succ_cluster=self.summmary.successor(h)
+                succ_cluster = None
+                if self.summary != None:
+                    succ_cluster = self.summary.successor(h)
                 if succ_cluster == None:
                     return None
                 else:
-                    cluster2=self.clusters[succ_cluster]
-                    offset=0
+                    cluster2 = self.clusters[succ_cluster]
+                    offset = 0
                     if cluster2 != None:
-                        offset=cluster2.min
-                    return self.index(succ_cluster,offset)
+                        offset = cluster2.min
+                    return self.index(succ_cluster, offset)
 
-
-    def predecessor(self,x):
-        if self.u<=2:
-            if x==0 and self.max==1:
+    def predecessor(self, x):
+        if self.u <= 2:
+            if x == 1 and self.min == 0:
                 return 1
             else:
                 return None
-        elif self.max!=None and x>self.max:
+        elif self.max != None and x > self.max:
             return self.max
         else:
-            h=self.high(x)
-            l=self.low(l)
-            minLow=None
-            cluster=self.clusters[h]
-            if self.clusters != None:
-                minLow=cluster.min
-            if minLow!=None and minLow<l:
-                offset=cluster.predecessor(l)
-                if offset == None:
-                    offset=0
-                return self.index(h,offset)
+            h = self.high(x)
+            l = self.low(x)
+            minLow = None
+            cluster = self.clusters[h]
+            if cluster != None:
+                minLow = cluster.min
+            if minLow != None and l > minLow:
+                offset = cluster.predecessor(l)
+                return self.index(h, offset)
             else:
-                pred_cluster=None
-                if self.summary!=None:
-                    pred_cluster=self.summary.predecessor(h)
-                if pred_cluster == None:
-                    if self.min != None and x>self.min:
+                pre_cluster = None
+                if self.summary != None:
+                    pre_cluster = self.summary.predecessor(h)
+                if pre_cluster == None:
+                    if self.min != None and x > self.min:
                         return self.min
                     else:
                         return None
                 else:
-                    cluster=self.clusters[pred_cluster]
-                    offset=0
-                    if cluster2!=None:
-                        offset=cluster2.max
-                    return self.index(pred_cluster,offset)
+                    cluster2 = self.clusters[pre_cluster]
+                    offset = 0
+                    if cluster2 != None:
+                        offset = cluster2.max
+                    return self.index(pre_cluster, offset)
 
-    def delete(self,x):
-        if x==self.min:
-            # Finding new min
-            i=self.summary.min
-            if i==None:
-                self.min=None
-                self.max=None
-                return
-            self.min=self.index(i,self.clusters[i].min)
-        # if x is not equal to self.min then we have to call the clusters and delete l index
-        h=self.high(x)
-        l=self.low(x)
-        self.clusters[h].delete(l)
-        if self.clusters[h].min==None:
-            # Second call to delete the info from summary
-            self.summary.delete(h)
-        # If x equals to self.max
-        if x==self.max:
-            # If we deleted last second item i.e. only self.min is remaining
-            if self.summary.max==None:
-                self.max=self.min
+    def delete(self, x):
+        if self.min == self.max:
+            self.min = None
+            self.max = None
+        elif self.u <= 2:
+            if x == 0:
+                self.min = 1
             else:
-                i=self.summary.max
-                self.max=self.index(i,self.clusters[i].max)
+                self.min = 0
+            self.max = self.min
+        else:
+            if x == self.min:
+                if self.summary != None:
+                    first_cluster = self.summary.min
+                    offset = self.clusters[first_cluster].min
+                    x = self.index(first_cluster, offset)
+                    self.min = x
+                else:
+                    raise Exception(' Value is not in the structure')
+            h = self.high(x)
+            l = self.low(x)
+            self.clusters[h].delete(l)
+            if self.clusters[h].min == None:
+                self.summary.delete(h)
+                if x == self.max:
+                    summary_max = self.summary.max
+                    if summary_max == None:
+                        self.max = self.min
+                    else:
+                        off = self.clusters[summary_max].max
+                        self.max = self.index(summary_max, off)
+            elif x == self.max:
+                off = self.clusters[self.high(x)].max
+                self.max = self.index(self.high(x), off)
 
+
+if __name__ == '__main__':
+    my_veb = VEB(32)
+    arr = [2, 3, 4, 5, 7, 14, 15, 27, 29]
+    for item in arr:
+        my_veb.insert(item)
+
+    print my_veb.member(27)
+    print my_veb.member(7)
+    my_veb.delete(2)
+    print my_veb.min
+    print my_veb.member(2)
